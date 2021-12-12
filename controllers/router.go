48272dc7d7d8cea8138f134333eb57e575f73e33
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-12 22:32:12 +0800

    feat(router): router 基本配置

---
+++ controllers/router.go
@@ -0,0 +1,78 @@
+package controllers
+
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/context"
+	"github.com/mlogclub/simple"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"xupt-scholarship/middleware"
+)
+
+func Router() {
+	app := iris.New()
+	app.Logger().SetLevel("warn")
+	middleware.UseMiddleWare(app)
+	app.AllowMethods(iris.MethodOptions)
+
+	app.OnAnyErrorCode(func(ctx *context.Context) {
+		path := ctx.Path()
+		var err error
+		if strings.Contains(path, "/api/admin") {
+			_, err = ctx.JSON(simple.JsonErrorCode(ctx.GetStatusCode(), "Http Error"))
+		}
+
+		if err != nil {
+			logrus.Error(err)
+		}
+	})
+
+	app.Any("/", func(c *context.Context) {
+		_, _ = c.HTML("<h1>Powered by xupt-scholarshipt@baiziyu-fe - | ©2022</h1>")
+	})
+
+	app.Get("/", func(context *context.Context) {
+		context.HTML("<h1>Hello World!!!</h1>")
+	})
+
+	server := &http.Server{Addr: ":8096"}
+	handleSignal(server)
+	err := app.Run(iris.Server(server), iris.WithConfiguration(iris.Configuration{
+		DisableStartupLog:                 false,
+		DisableInterruptHandler:           false,
+		DisablePathCorrection:             false,
+		EnablePathEscape:                  false,
+		FireMethodNotAllowed:              false,
+		DisableBodyConsumptionOnUnmarshal: false,
+		DisableAutoFireStatusCode:         false,
+		EnableOptimizations:               true,
+		TimeFormat:                        "2006-01-02 15:04:05",
+		Charset:                           "UTF-8",
+	}))
+
+	if err != nil {
+		logrus.Error(err)
+		os.Exit(-1)
+	}
+
+}
+
+func handleSignal(server *http.Server) {
+	c := make(chan os.Signal)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+
+	go func() {
+		s := <-c
+		logrus.Infof("got signal [%s], exiting now!!!", s)
+		if err := server.Close(); err != nil {
+			logrus.Errorf("server close failed: " + err.Error())
+		}
+		simple.CloseDB()
+		logrus.Infof("Exited")
+		os.Exit(0)
+	}()
+}
--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/context"
-	"xupt-scholarship/middleware"
-)
+import "xupt-scholarship/controllers"
 
 func main() {
-	app := iris.New()
-	middleware.UseMiddleWare(app)
-	app.Get("/", func(context *context.Context) {
-		context.HTML("<h1>Hello World!!!</h1>")
-	})
-	app.Listen(":8096")
+	controllers.Router()
 }
