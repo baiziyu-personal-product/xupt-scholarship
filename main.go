0efe8730194ba59afa2f60a2c05dbeae2573ffbb
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-13 15:54:25 +0800

    feat(serve): 添加提示

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/fatih/color"
 	"xupt-scholarship/controllers"
 	"xupt-scholarship/initialize"
 )
 
 func main() {
 	initialize.InitServeConfig()
+
+	color.Green("Serve config has been emitted!!!")
 	controllers.Router()
 }
