1f81c7b0782472484a531d1cf0a4e61502e9abae
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2022-01-31 23:53:55 +0800

    feat(db): application_form

--- db/struct.go
+++ db/struct.go
@@ -1,10 +1,15 @@
 package db
 
-type Students struct {
-	Name       string `gorm:"column:name"`
-	Email      string `gorm:"column:email"`
-	Phone      string `gorm:"column:phone"`
-	Password   string `gorm:"column:password"`
-	AccessType int    `gorm:"column:access_type"`
-	Access     string `gorm:"column:access"`
+// ApplicationForm 奖学金申请表
+type ApplicationForm struct {
+	Id string `gorm:"column:id"`
+	CreateTime string `gorm:"column:create_time"`
+	StudentId string `gorm:"column:student_id"`
+	Info string `gorm:"column: info"`
+	Editable int `gorm:"column: editable"`
+	UpdateTime string `gorm:"column: update_time"`
+	Year int `gorm:"column: year"`
+	Archive int `gorm:"column: archive"`
+	ProcessId int `gorm:"column: process_id"`
+	ListId int `gorm:"column: list_id"`
 }
