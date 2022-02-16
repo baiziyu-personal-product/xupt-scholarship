5bcb16b1fdf0e7151736ae6da6983ae56797e090
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2022-02-16 21:05:22 +0800

    feat(verify code): 验证

--- controllers/captcha.go
+++ controllers/captcha.go
@@ -6,72 +6,57 @@ import (
 	"github.com/kataras/iris/v12/mvc"
 	"net/http"
 	"strings"
-	"text/template"
 	"time"
 )
 
 type Captcha struct {
 	BaseController
-	code string
 }
 
 func CaptchaMVC(app *mvc.Application) {
 	app.Handle(new(Captcha))
 }
 
-func (C *Captcha) GetBy(code string) {
+func (C *Captcha) GetBy(imgPath string) {
 	w, r := C.Ctx.ResponseWriter(), C.Ctx.Request()
-	C.code = code
-	point := strings.Index(code, ".")
+	point := strings.Index(imgPath, ".")
 	var content bytes.Buffer
 	w.Header().Set("Content-Type", "image/png")
-	captcha.WriteImage(&content, code[:point], captcha.StdWidth, captcha.StdWidth)
-	http.ServeContent(w, r, code, time.Time{}, bytes.NewReader(content.Bytes()))
+	code := imgPath[:point]
+	captcha.WriteImage(&content, code, captcha.StdWidth, captcha.StdHeight)
+	http.ServeContent(w, r, imgPath, time.Time{}, bytes.NewReader(content.Bytes()))
 }
 
-func (C *Captcha) Get() http.Handler {
-	w := C.Ctx.ResponseWriter()
-	//if r.URL.Path != "/" {
-	//	http.NotFound(w, r)
-	//	return nil
-	//}
-	d := struct {
-		CaptchaId string
-	}{
-		captcha.New(),
+type VerifyData struct {
+	Code      string `json:"code"`
+	InputCode string `json:"input_code"`
+}
+
+// PostVerify 判断验证码是否正确
+func (C *Captcha) PostVerify() ResponseFmtData {
+	var data VerifyData
+	GetRequestParams(C.Ctx, &data)
+	return ResponseFmtData{
+		Message: "verifyed",
+		Code:    0,
+		Data:    captcha.VerifyString(data.Code, data.InputCode),
 	}
-	if err := formTemplate.Execute(w, &d); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+
+func (C *Captcha) Get() ResponseFmtData {
+	captchaId := captcha.New()
+	return ResponseFmtData{
+		Message: "success",
+		Code:    0,
+		Data:    captchaId,
 	}
-	return captcha.Server(captcha.StdWidth, captcha.StdHeight)
 }
 
-var formTemplate = template.Must(template.New("example").Parse(formTemplateSrc))
-
-const formTemplateSrc = `<!doctype html>
-<head><title>Captcha Example</title></head>
-<body>
-<script>
-function setSrcQuery(e, q) {
-    var src  = e.src;
-    var p = src.indexOf('?');
-    if (p >= 0) {
-        src = src.substr(0, p);
-    }
-    e.src = src + "?" + q
+func (C *Captcha) GetReload() ResponseFmtData {
+	code := C.Ctx.URLParam("code")
+	return ResponseFmtData{
+		Message: "success",
+		Code:    0,
+		Data:    captcha.Reload(code),
+	}
 }
-function reload() {
-    setSrcQuery(document.getElementById('image'), "reload=" + (new Date()).getTime());
-    setSrcQuery(document.getElementById('audio'), (new Date()).getTime());
-    return false;
-}
-</script>
-<form action="/process" method=post>
-<p>Type the numbers you see in the picture below:</p>
-<p><img id=image src="/captcha/{{.CaptchaId}}.png" alt="Captcha image"></p>
-<a href="#" onclick="reload()">Reload</a> 
-<input type=hidden name=captchaId value="{{.CaptchaId}}"><br>
-<input name=captchaSolution>
-<input type=submit value=Submit>
-</form>
-`
