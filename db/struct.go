7eedb58f4df88550428e2acf8dfd8196eeab7a86
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2022-02-28 13:24:10 +0800

    feat(db): struct

--- db/struct.go
+++ db/struct.go
@@ -11,3 +11,16 @@ type User struct {
 	ManageId  string `gorm:"column:manage_id"`
 	StudentId string `gorm:"column:student_id"`
 }
+
+// Application 申请表单
+type Application struct {
+	Form      interface{} `gorm:"column:form"`
+	History   interface{} `gorm:"column:history"`
+	CreateAt  int64       `gorm:"column:create_at"`
+	EditAt    int64       `gorm:"column:edit_at"`
+	Score     interface{} `gorm:"column:score"`
+	StudentId string      `gorm:"column:student_id"`
+	Status    int         `gorm:"column:status"`
+	Step      string      `gorm:"column:step"`
+	Year      int         `gorm:"column:year"`
+}
